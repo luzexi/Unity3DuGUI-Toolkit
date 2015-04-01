f59d4f54f46de97046eb145ee419f3b1ad94c9b3
陆泽西 <jesse_luzexi@163.com>
2015-04-02 00:16:55 +0800

    add init font

---
+++ UI_FontNum.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//  UI_FontNum.cs
+//  Author: Lu Zexi
+//  2015-03-31
+
+
+
+//font num
+public class UI_FontNum : MonoBehaviour
+{
+    public Texture2D[] Num;
+
+    public void Setup( int num )
+    {
+        List<int> lst = new List<int>();
+
+        // for(;num;)
+    }
+}
+
